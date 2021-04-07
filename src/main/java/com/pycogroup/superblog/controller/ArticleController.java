b615bab5db461153001ffaf6f31f8530d5a3bb2e
Thanh Tai <thanhtai1789@gmail.com>
2021-04-07 13:15:05 +0700

    update Article Controller

--- src/main/java/com/pycogroup/superblog/controller/ArticleController.java
+++ src/main/java/com/pycogroup/superblog/controller/ArticleController.java
@@ -1,10 +1,9 @@
 package com.pycogroup.superblog.controller;
 
 import com.pycogroup.superblog.api.ArticlesApi;
-import com.pycogroup.superblog.api.model.ArticleListResponse;
-import com.pycogroup.superblog.api.model.ObjectCreationSuccessResponse;
-import com.pycogroup.superblog.api.model.CreateArticleRequest;
+import com.pycogroup.superblog.api.model.*;
 import com.pycogroup.superblog.model.Article;
+import com.pycogroup.superblog.model.Comment;
 import com.pycogroup.superblog.service.ArticleService;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
@@ -32,8 +31,56 @@ public class ArticleController implements ArticlesApi {
 	}
 
 	@Override
-	public ResponseEntity<ObjectCreationSuccessResponse> createArticle(@Valid CreateArticleRequest createArticleRequest) {
-		return null;
+	public ResponseEntity<ObjectCreationSuccessResponse> createArticle(@Valid CreateArticleRequest createArticleRequest)  {
+		Article article = modelMapper.map(createArticleRequest, Article.class);
+		articleService.createArticle(article);
+		ObjectCreationSuccessResponse result = new ObjectCreationSuccessResponse();
+		result.setId(article.getArticleId());
+		result.setResponseCode(HttpStatus.CREATED.value());
+		return new ResponseEntity<>(result, HttpStatus.CREATED);
+	}
+
+	@Override
+	public ResponseEntity<ObjectCreationSuccessResponse> createComment(String articleId, @Valid CreateCommentRequest createCommentRequest) {
+		Comment comment =  modelMapper.map(createCommentRequest, Comment.class);
+		String commentId = articleService.createComment(articleId, comment);
+		ObjectCreationSuccessResponse result = new ObjectCreationSuccessResponse();
+		result.setId(commentId);
+		result.setResponseCode(HttpStatus.CREATED.value());
+		return new ResponseEntity<>(result, HttpStatus.CREATED);
+	}
+
+	// Can only update article title, content, categories
+	// Update any of above fields if it is not null
+	@Override
+	public ResponseEntity<ObjectCreationSuccessResponse> updateArticle(String articleId, @Valid UpdateArticleRequest updateArticleRequest) {
+		Article article = modelMapper.map(updateArticleRequest, Article.class);
+		articleService.updateArticle(articleId, article);
+		ObjectCreationSuccessResponse result = new ObjectCreationSuccessResponse();
+		result.setId(articleId);
+		result.setResponseCode(200);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	// Only allow article author can update status of comment
+	@Override
+	public ResponseEntity<ObjectCreationSuccessResponse> updateComment(String articleId, String commentId, @Valid UpdateCommentRequest updateCommentRequest) {
+		Comment comment = modelMapper.map(updateCommentRequest, Comment.class);
+		articleService.updateComment(articleId, commentId, updateCommentRequest.getAuthorEmail() ,comment);
+		ObjectCreationSuccessResponse result = new ObjectCreationSuccessResponse();
+		result.setId(commentId);
+		result.setResponseCode(200);
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
+
+	@Override
+	public ResponseEntity<ObjectCreationSuccessResponse> deleteArticle(String articleId) {
+		articleService.deleteArticle(articleId);
+		ObjectCreationSuccessResponse result = new ObjectCreationSuccessResponse();
+		// TODO
+		result.setId(articleId);
+		result.setResponseCode(HttpStatus.OK.value());
+		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
 	private ResponseEntity<ArticleListResponse> buildArticleListResponse(List<Article> articleList) {
