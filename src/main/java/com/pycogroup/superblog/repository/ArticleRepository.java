f83280b37a29761b596bdead29bd1affd8cb66f2
Thanh Tai <thanhtai1789@gmail.com>
2021-04-07 13:16:44 +0700

    update Article Repository

--- src/main/java/com/pycogroup/superblog/repository/ArticleRepository.java
+++ src/main/java/com/pycogroup/superblog/repository/ArticleRepository.java
@@ -1,9 +1,23 @@
 package com.pycogroup.superblog.repository;
 
 import com.pycogroup.superblog.model.Article;
+import com.pycogroup.superblog.model.Comment;
 import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface ArticleRepository extends MongoRepository<Article, ObjectId> {
+import java.util.Optional;
 
+public interface ArticleRepository extends MongoRepository<Article, ObjectId>, QuerydslPredicateExecutor<Article> {
+
+    Article findArticleByArticleId(String articleId);
+
+    Optional<Article> findArticleByTitle(String title);
+
+    //maybe need to add , fields = "{ 'comments.commentId' : 1 }"
+    @Query(value = "{ 'articleId' : ?0, 'comments.commentId' : ?1 }")
+    Optional<Article> findByArticleIdAndCommentsCommentId(String articleId, String commentId);
+
+    void deleteByArticleId(String articleId);
 }
