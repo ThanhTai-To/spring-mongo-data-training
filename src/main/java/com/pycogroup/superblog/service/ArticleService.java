878b0cff11877bc24fb615f4b3d231939bad5c36
Thanh Tai <thanhtai1789@gmail.com>
2021-04-07 13:15:30 +0700

    update Article Service

--- src/main/java/com/pycogroup/superblog/service/ArticleService.java
+++ src/main/java/com/pycogroup/superblog/service/ArticleService.java
@@ -1,10 +1,17 @@
 package com.pycogroup.superblog.service;
 
 import com.pycogroup.superblog.model.Article;
+import com.pycogroup.superblog.model.Comment;
 
 import java.util.List;
 
 public interface ArticleService {
 	List<Article> getAllArticles();
 	Article createArticle(Article article);
+	String createComment(String articleId, Comment comment);
+	void updateArticle(String articleId, Article article);
+	void updateComment(String articleId, String commentId, String updateCommentAuthorEmail,Comment comment);
+	void deleteArticle(String articleId);
+
+
 }
